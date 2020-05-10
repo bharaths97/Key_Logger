8f50324ce1811f341ae52f13d3fe26e9affef53c
Bharath Suresh <bharathsur97@gmail.com>
2020-05-10 19:39:07 +0530

    Initial Header and Source File

---
+++ Source/KeyLogger.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
